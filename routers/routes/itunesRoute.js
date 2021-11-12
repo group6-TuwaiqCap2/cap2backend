717f17c7712556196837b5b44ebf61514135188d
Ghadier-Alenezi <Ghadier.Mutar.elq.js@tuwaiq.edu.sa>
2021-11-12 21:49:29 +0300

    edit brackes

--- routers/routes/itunesRoute.js
+++ routers/routes/itunesRoute.js
@@ -65,10 +65,10 @@ const getAllebookMiddleware = (req, res, next) => {
 const ebookRouter = express.Router();
 ebookRouter.get("/", getAllebookMiddleware, getAllebook);
 
-(module.exports = movieRouter),
+(module.exports = movieRouter,
   podcastRouter,
   musicRouter,
   audiobookRouter,
   tvShowRouter,
   softwareRouter,
-  ebookRouter;
+  ebookRouter);
