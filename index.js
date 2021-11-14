ebf03aea452818672fb56a076743d73b934e5545
Mohammedali18 <mod8920@gmail.com>
2021-11-14 23:23:38 +0300

    Fav

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const helmet = require("helmet");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const axios = require("axios");
+const fs = require("fs");
 
 dotenv.config();
 const app = express();
@@ -12,6 +13,16 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
 app.use(helmet());
+
+let faiveredmovie = [];
+fs.readFile("./db/faiverotmovie.json", (err, data) => {
+  if (err) {
+    console.log(err);
+    return err;
+  } else {
+    faiveredmovie = JSON.parse(data.toString());
+  }
+});
 
 app.get("/movie", (req, res) => {
   axios
@@ -76,6 +87,25 @@ app.get("/ebook", (req, res) => {
       res.status(200).json(response.data);
     });
 });
+
+//favoruite
+
+const addToFav2 = (req, res) => {
+  const { result } = req.body;
+  faiveredmovie.push(result);
+  fs.writeFile(
+    "./db/faiverotmovie.json",
+    JSON.stringify(faiveredmovie),
+    (err) => {
+      if (err) {
+        res.status(404).json("badresult");
+      } else {
+        res.status(200).json(result);
+      }
+    }
+  );
+};
+app.post("/movie/addToFav2", addToFav2);
 
 const PORT = process.env.PORT || 4000;
 
